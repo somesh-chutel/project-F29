d36e823d9e07c15e96c2f31fe50c0e5df17d1ed0
srocknik <somesh.chutel90@gmail.com>
2024-12-26 20:20:18 +0530

    first commit

---
+++ src/components/notFound/index.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+const NotFound = () => (
+  <div className="not-found-cont">
+    <img width="300px" src="https://cdni.iconscout.com/illustration/premium/thumb/not-found-illustration-download-in-svg-png-gif-file-formats--404-error-page-pack-network-communication-illustrations-6167023.png?f=webp" alt="" />
+
+    <br />
+    <br />
+
+    <h1 style={{textAlign:"center"}}>
+      The Path you are using is not the part of route please try again with the
+      correct path!!!!
+    </h1>
+  </div>
+);
+
+export default NotFound;
